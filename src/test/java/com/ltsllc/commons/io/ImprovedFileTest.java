531914f9b9120b7d47b6dd721af4a7738ab768f7
clark <clark.hobbie@gmail.com>
2025-12-15 13:59:11 -0700

    added clear method to ImprovedFile

---
+++ src/test/java/com/ltsllc/commons/io/ImprovedFileTest.java
@@ -0,0 +1,37 @@
+package com.ltsllc.commons.io;
+
+import org.junit.jupiter.api.Test;
+
+public class ImprovedFileTest {
+    @Test
+    void createImprovedTempFile() {
+    }
+
+    @Test
+    void contentsEquals() {
+    }
+
+    @Test
+    void copyTo() {
+    }
+
+    @Test
+    void touch() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void backup() {
+    }
+
+    @Test
+    void testBackup() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
